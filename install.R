8c7f553f2dffdd4d6596af770818a4f6d0a96e43
David Chudzicki <dchudz@gmail.com>
2014-02-09 08:38:05 -0800

    removed clutter

--- install.R
+++
@@ -1,4 +0,0 @@
-
-library(devtools)
-install_github("predcomps", username="dchudz")
-library("predcomps")
--- main.R
+++
@@ -1,9 +0,0 @@
-library("devtools")
-setwd("~/predcomps")
-document()
-load_all()
-
-dev_help("Mahal")
-dev_help("GetPairs")
-dev_help("resample_from_pairs")
-dev_help("GetAPC")
